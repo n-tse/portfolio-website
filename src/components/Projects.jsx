2a20168e1d39129d78b9321a17eae4e4dcf32884
Nick Tse <nicholashtse@gmail.com>
2023-07-18 13:29:36 -0400

    update section styling and spacing to match About

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,15 +11,16 @@ import { projectsList } from "../data.js";
 
 const Projects = () => {
   return (
-    <section name="Projects" className="w-full md:h-screen bg-slate-400">
+    <section name="Projects" className="w-full py-12 min-h-fit flex flex-col justify-center items-center sm:px-14">
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+          <h2 className="text-4xl md:text-5xl lg:text-6xl font-bold inline border-b-4 border-slate-300">
+
             Projects
-          </p>
-          <p className="py-6">Check out some of my work!</p>
+          </h2>
+          <p className="pt-6 pb-2">Check out some of my work!</p>
         </div>
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-4">
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {projectsList.map(
             ({
               id,
