622a5c632e217f14acb2d2d22f5daca4cbde147c
Nick Tse <nicholashtse@gmail.com>
2023-11-01 17:50:12 -0700

    update resume

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -43,7 +43,7 @@ const SocialLinks = () => {
           Resume <FaUserTie size={35} color="#1D4384"/>
         </>
       ),
-      href: "/NicholasTse_Resume.32227.pdf",
+      href: "/NicholasTse_Resume.321011.pdf",
       customStyle: "rounded-br-md",
     },
   ];
